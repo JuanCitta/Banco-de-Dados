a0824e8f1107bb1f0e3d946034b155c694b06bba
Juan Citta <143374068+JuanCitta@users.noreply.github.com>
2025-04-13 00:30:47 -0300

    Script de Validacao
    Versao Inicial do script para validacao dos dados do banco.

---
+++ validar_dados.py
@@ -0,0 +1,159 @@
+import psycopg2
+
+ULTIMO_SEM = 2025.1
+
+def validar_dados():
+    try:
+        conn = psycopg2.connect(
+            host="db.gmuoinhzwkcxwoinnqit.supabase.co",
+            database="postgres",
+            user="postgres",
+            password="zGcUhOCaI1EUmLDX"
+        )
+        cursor = conn.cursor()
+
+        tabelas = [
+            "aluno", "disciplina", "professor", "curso", "departamento",
+            "curso_disciplina", "matriz_curricular", "escuta", "profere", "tcc", "tcc_aluno"
+        ]
+
+        dados = {}
+        for tabela in tabelas:
+            cursor.execute(f"SELECT * FROM {tabela}")
+            dados[tabela] = cursor.fetchall()
+
+        erros = []
+        ids_departamentos = set()
+        for linha in dados["departamento"]:
+            ids_departamentos.add(linha[0])
+
+        ids_cursos = set()
+        for linha in dados["curso"]:
+            ids_cursos.add(linha[0])
+
+        ids_alunos = set()
+        for linha in dados["aluno"]:
+            ids_alunos.add(linha[0])
+
+        ids_disciplinas = set()
+        for linha in dados["disciplina"]:
+            ids_disciplinas.add(linha[0])
+
+        ids_professores = set()
+        for linha in dados["professor"]:
+            ids_professores.add(linha[0])
+
+        ids_tccs = set()
+        for linha in dados["tcc"]:
+            ids_tccs.add(linha[0])
+
+        for linha in dados["aluno"]:
+            ra = linha[0]
+            id_curso = linha[2]
+            if id_curso not in ids_cursos:
+                erros.append(f"Aluno RA {ra} tem curso invalido: {id_curso}")
+
+        for linha in dados["disciplina"]:
+            id_disciplina = linha[0]
+            id_departamento = linha[2]
+            if id_departamento not in ids_departamentos:
+                erros.append(f"Disciplina {id_disciplina} tem curso invalido: {id_curso}")
+
+        for linha in dados["professor"]:
+            id_professor = linha[0]
+            id_departamento = linha[2]
+            if id_departamento not in ids_departamentos:
+                erros.append(f"Professor {id_professor} tem departamento invalido: {id_departamento}")
+
+        for linha in dados["curso"]:
+            id_curso = linha[0]
+            id_coordenador = linha[2]
+            if id_coordenador not in ids_professores:
+                erros.append(f"Curso {id_curso} tem departamento invalido: {id_departamento}")
+
+        for id_disciplina, id_curso in dados["curso_disciplina"]:
+            if id_curso not in ids_cursos:
+                erros.append(f"Curso_disciplina: curso invalido {id_curso}")
+            if id_disciplina not in ids_disciplinas:
+                erros.append(f"Curso_disciplina: disciplina invalida {id_disciplina}")
+
+        for id_curso, id_disciplina, semestre in dados["matriz_curricular"]:
+            if id_curso not in ids_cursos:
+                erros.append(f"Matriz_curricular: curso invalido {id_curso}")
+            if id_disciplina not in ids_disciplinas:
+                erros.append(f"Matriz_curricular: disciplina invalida {id_disciplina}")
+        
+        DP = []
+        for linha in dados["escuta"]:
+            ra = linha[0]
+            id_disciplina = linha[1]
+            nota = linha[3]
+            ciclo = linha[4]
+            if ra not in ids_alunos:
+                erros.append(f"Escuta: RA invalido {ra}")
+            if id_disciplina not in ids_disciplinas:
+                erros.append(f"Escuta: disciplina invalida {id_disciplina}")
+            if nota < 5:
+                DP.append(linha)
+
+        for ra, id_disciplina, sem, nota, ciclo in DP:
+            fez_DP = False
+        for linha in dados["escuta"]:
+            ra2 = linha[0]
+            id_disciplina2 = linha[1]
+            sem2 = linha[2]
+            nota2 = float(linha[3])
+
+            if ra == ra2 and id_disciplina == id_disciplina2:
+                if float(sem2) > float(sem) and nota2 >= 5:
+                    fez_DP = True
+                    break
+
+        if not fez_DP and float(sem) != 2025.1:
+            erros.append(f"Aluno {ra} reprovou e nao refez a disciplina {id_disciplina} {sem}")
+
+                    
+
+        for linha in dados["profere"]:
+            id_professor = linha[0]
+            id_disciplina = linha[1]
+            semestre = linha[2]
+            if id_professor not in ids_professores:
+                erros.append(f"Profere: professor invalido {id_professor}")
+            if id_disciplina not in ids_disciplinas:
+                erros.append(f"Profere: disciplina invalida {id_disciplina}")
+            if float(semestre)>= 2025.2 :
+                erros.append(f"Ano ou semestre invalido Semestre:{semestre}")
+                
+
+        for linha in dados["tcc"]:
+            id_tcc = linha[0]
+            id_professor = linha[1]
+            semestre = linha[4]
+            if id_professor not in ids_professores:
+                erros.append(f"TCC {id_tcc}: professor invalido {id_professor}")
+            if semestre is None or str(semestre).strip() == "":
+                erros.append(f"TCC {id_tcc}: semestre esta vazio")
+
+        for linha in dados["tcc_aluno"]:
+            id_tcc = linha[0]
+            ra = linha[1]
+            if id_tcc not in ids_tccs:
+                erros.append(f"TCC_aluno: id_tcc invalido {id_tcc}")
+            if ra not in ids_alunos:
+                erros.append(f"TCC_aluno: RA invalido {ra}")
+
+        if len(erros) > 0:
+            print("Erros de consistencia encontrados:")
+            for erro in erros:
+                print(" -", erro)
+        else:
+            print("Todas as tabelas estao consistentes.")
+
+    except Exception as e:
+        print(f"Erro: {e}")
+        
+    finally:
+        if 'conn' in locals():
+            conn.close()
+validar_dados()
